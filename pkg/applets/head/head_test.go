529c9781586f05b6c2b397e193cc74a7fc3953b0
Rui Carmo <rui.carmo@gmail.com>
2026-02-01 18:49:16 +0000

    Add head and tail tests

---
+++ pkg/applets/head/head_test.go
@@ -0,0 +1,69 @@
+package head_test
+
+import (
+	"testing"
+
+	"github.com/rcarmo/busybox-wasm/pkg/applets/head"
+	"github.com/rcarmo/busybox-wasm/pkg/core"
+	"github.com/rcarmo/busybox-wasm/pkg/testutil"
+)
+
+func TestHead(t *testing.T) {
+	tests := []testutil.AppletTestCase{
+		{
+			Name:     "default_lines",
+			Args:     []string{"test.txt"},
+			WantCode: core.ExitSuccess,
+			WantOut:  "a\nb\nc\n",
+			Files: map[string]string{
+				"test.txt": "a\nb\nc\n",
+			},
+		},
+		{
+			Name:     "limit_lines",
+			Args:     []string{"-n", "2", "test.txt"},
+			WantCode: core.ExitSuccess,
+			WantOut:  "a\nb\n",
+			Files: map[string]string{
+				"test.txt": "a\nb\nc\n",
+			},
+		},
+		{
+			Name:     "byte_count",
+			Args:     []string{"-c", "4", "bytes.txt"},
+			WantCode: core.ExitSuccess,
+			WantOut:  "abcd",
+			Files: map[string]string{
+				"bytes.txt": "abcdef",
+			},
+		},
+		{
+			Name:     "multi_header",
+			Args:     []string{"file1.txt", "file2.txt"},
+			WantCode: core.ExitSuccess,
+			WantOut:  "==> file1.txt <==\na\nb\n\n==> file2.txt <==\nc\n",
+			Files: map[string]string{
+				"file1.txt": "a\nb\n",
+				"file2.txt": "c\n",
+			},
+		},
+		{
+			Name:     "quiet_mode",
+			Args:     []string{"-q", "file1.txt", "file2.txt"},
+			WantCode: core.ExitSuccess,
+			WantOut:  "a\nb\nc\n",
+			Files: map[string]string{
+				"file1.txt": "a\nb\n",
+				"file2.txt": "c\n",
+			},
+		},
+		{
+			Name:     "missing_file",
+			Args:     []string{"/missing"},
+			WantCode: core.ExitFailure,
+			WantErr:  "head:",
+		},
+	}
+
+	testutil.RunAppletTests(t, head.Run, tests)
+}
